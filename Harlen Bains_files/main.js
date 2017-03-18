c61b1a50807aa97ff4e08b9361a12c65eb1d75c3
Harlen Bains <himalayanelixir@gmail.com>
2017-03-18 16:58:00 -0700

    Not sure.

--- Harlen Bains_files/main.js
+++ Harlen Bains_files/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $.simpleWeather({
-    location: 'Elk Grove, CA',
+    location: 'San Jose, CA',
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<p class="animated fadeInUp">'+'Creating software applications in Elk Grove, California where it&#39;s '+weather.temp+'&deg;'+' and sunny. <a href="mailto:harlenbains@ieee.org">Contact Me</a>.'+'</p>';
-  
+      html = '<p class="animated fadeInUp">'+'Creating software applications in San Jose, California where it&#39;s '+weather.temp+'&deg;'+' and beautiful. <a href="mailto:harlenbains@ieee.org">Contact Me</a>.'+'</p>';
+
       $("#weather").html(html);
     },
     error: function(error) {
