428be2192f203647ffcca5542b068c5588388400
Harlen Bains <himalayanelixir@gmail.com>
2017-10-29 22:10:41 -0700

    Changed Contact Link

--- Harlen Bains_files/main.js
+++ Harlen Bains_files/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<p class="animated fadeInUp">'+'Creating software applications in San Jose, California where it&#39;s '+weather.temp+'&deg;'+' and beautiful. <a href="mailto:harlenbains@ieee.org">Contact Me</a>.'+'</p>';
+      html = '<p class="animated fadeInUp">'+'Creating software applications in San Jose, California where it&#39;s '+weather.temp+'&deg;'+' and beautiful. <a href="https://www.linkedin.com/in/harlenbains/">Contact Me</a>.'+'</p>';
 
       $("#weather").html(html);
     },
