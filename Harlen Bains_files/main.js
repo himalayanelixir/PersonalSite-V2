b097736c839e55007e1d256c8fc39904086a558a
Harlen Bains <himalayanelixir@gmail.com>
2019-04-22 21:58:46 -0700

    Removed weather and updated flavicon

--- Harlen Bains_files/main.js
+++ Harlen Bains_files/main.js
@@ -4,9 +4,7 @@ $(document).ready(function() {
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<p class="animated fadeInUp">'+'Creating software applications in San Jose, California where it&#39;s '+weather.temp+'&deg;'+' and beautiful. <a href="https://www.linkedin.com/in/harlenbains/">Contact Me</a>.'+'</p>';
-
-      $("#weather").html(html);
+      html = '<p class="animated fadeInUp">'+'Creating software applications in San Jose, California. <a href="https://www.linkedin.com/in/harlenbains/">Contact Me</a>.'+'</p>';
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
